3eccf28706852977158dbb03c9b7f2c9df5f0052
Isabell Helling <isabell.helling@gmail.com>
2024-05-31 22:35:39 +0200

    show GymAccountForm with first name field

--- frontend/src/pages/CreateGymAccount.tsx
+++ frontend/src/pages/CreateGymAccount.tsx
@@ -1,22 +1,12 @@
-import { useState } from 'react'
+import GymAccountForm from "@/components/GymAccountForm";
+
+function storeGymAccountInDB() {}
 
 function CreateGymAccount() {
-    const [count, setCount] = useState(0)
-  
     return (
       <>
-        <h1>You can create a gym account here! :D</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            {count} gym accounts created
-          </button>
-          <p>
-            Great to see you here! You can create a gym account by clicking the button above.
-          </p>
-        </div>
-        <p className="great-day">
-          I am glad you can see this page. Have a great day!
-        </p>
+        <h1>Create your parter account</h1>
+        <GymAccountForm />
       </>
     )
   }
