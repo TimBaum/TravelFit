7aac7e064aa8fabd1292fe5d34ff6b9e0ce46993
Isabell Helling <isabell.helling@gmail.com>
2024-06-01 09:19:26 +0200

    add navigation bar to createGymAccount for testing

--- frontend/src/pages/CreateGymAccount.tsx
+++ frontend/src/pages/CreateGymAccount.tsx
@@ -1,9 +1,11 @@
 import GymAccountForm from "@/components/GymAccountForm";
 import { Button } from "@/components/ui/button";
+import { NavigationMenuGym } from "@/components/ui/navigation-menu";
 
 function CreateGymAccount() {
     return (
       <>
+      <NavigationMenuGym/>
         <h1>Create your partner account</h1>
         <p>Attract a broad audience</p>
         <p>Spread information about offers and subscriptions</p>
