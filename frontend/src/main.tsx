b31bffb16209611194fee29d249a7ef245fc605d
Isabell Helling <isabell.helling@gmail.com>
2024-06-01 15:58:21 +0200

    add AddGym to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MyGyms from './pages/MyGyms.tsx'
-<<<<<<< HEAD
-=======
 import AddGym from './pages/AddGym.tsx'
->>>>>>> manage-gyms
 
 import './index.css'
 import CreateGymAccount from './pages/CreateGymAccount.tsx'
@@ -12,13 +9,9 @@ import CreateUserAccount from './pages/CreateUserAccount.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<<<<<<< HEAD
   <CreateGymAccount />
   <CreateUserAccount />
   <MyGyms />
-=======
-    <MyGyms />
-    <AddGym />
->>>>>>> manage-gyms
+  <AddGym />
   </React.StrictMode>,
 )
