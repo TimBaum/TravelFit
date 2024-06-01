74eb4c026217aabfe02208b8ccf6d72cd5c9e7ee
Isabell Helling <isabell.helling@gmail.com>
2024-06-01 15:31:47 +0200

    add createGymAccount page and createUserAccount page to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,12 @@ import MyGyms from './pages/MyGyms.tsx'
 import { NavigationMenuUser } from './components/ui/navigation-menu.tsx'
 
 import './index.css'
+import CreateGymAccount from './pages/CreateGymAccount.tsx'
 import CreateUserAccount from './pages/CreateUserAccount.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-   <CreateUserAccount />
+  <CreateGymAccount />
+  <CreateUserAccount />
   </React.StrictMode>,
 )
--- frontend/src/pages/CreateGymAccount.tsx
+++ frontend/src/pages/CreateGymAccount.tsx
@@ -29,5 +29,5 @@ function CreateGymAccount() {
       </>
     )
   }
-  
+
   export default CreateGymAccount  
