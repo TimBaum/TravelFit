0c3815e49a593203602502910267b788245da06f
Isabell Helling <isabell.helling@gmail.com>
2024-06-01 15:35:24 +0200

    add MyGyms page to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MyGyms from './pages/MyGyms.tsx'
-import { NavigationMenuUser } from './components/ui/navigation-menu.tsx'
 
 import './index.css'
 import CreateGymAccount from './pages/CreateGymAccount.tsx'
@@ -11,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
   <CreateGymAccount />
   <CreateUserAccount />
+  <MyGyms />
   </React.StrictMode>,
 )
