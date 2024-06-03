216e0d8c5bdf56bf699db9bc0c3b4573316f9bec
Tim Baum <79936444+TimBaum@users.noreply.github.com>
2024-06-03 16:34:12 +0200

    add basic routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MyGyms from './pages/MyGyms.tsx'
-import AddGym from './pages/AddGym.tsx'
-
 import './index.css'
-import CreateGymAccount from './pages/CreateGymAccount.tsx'
-import CreateUserAccount from './pages/CreateUserAccount.tsx'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-  <CreateGymAccount />
-  <CreateUserAccount />
-  <MyGyms />
-  <AddGym />
+    <App />
   </React.StrictMode>,
 )
---
+++ frontend/src/pages/GymSearch.tsx
@@ -0,0 +1,10 @@
+function GymSearch() {
+  return (
+    <div>
+      <h1>Gym Search</h1>
+      <p>Find a gym near you</p>
+    </div>
+  )
+}
+
+export default GymSearch
