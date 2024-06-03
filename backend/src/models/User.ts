19eb1bafa5c02c9dd96bb61aa4d43d9da560f046
Isabell Helling <isabell.helling@gmail.com>
2024-06-03 10:52:47 +0200

    adjust user schema

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -3,13 +3,13 @@ import mongoose from 'mongoose';
 import Payment from './Payment';
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, index: true },
-  displayName: String,
-  picture: String,
-  salutation: { type: String, enum: ["Mr.", "Ms.", "Diverse"] },
-  password: String,
-  hasPremiumSubscription: Boolean,
-  payments: [Payment],
+  email: { type: String, required: true, index: true },
+  displayName: {type: String, required: true},
+  picture: {type: String, required: false},
+  salutation: { type: String, required: true, enum: ["Mr.", "Ms.", "Diverse"] },
+  password: {type: String, required: true},
+  hasPremiumSubscription: {type: Boolean, required: true},
+  payments: {type: [Payment], required: false},
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
