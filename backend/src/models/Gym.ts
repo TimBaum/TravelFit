14c193e765cdfff478aad6b801fdecce555fbc2e
Tim Baum <79936444+TimBaum@users.noreply.github.com>
2024-06-04 12:02:21 +0200

    fix gym schema

--- backend/src/models/Gym.ts
+++ backend/src/models/Gym.ts
@@ -1,10 +1,10 @@
 // backend/src/models/Gym.js
-import mongoose, { Document, Schema } from 'mongoose';
-import Adress from './Adress';
-import TimeRange from './TimeRange';
-import Offer from './Offer';
-import Review from './Review';
-import { ObjectId } from 'mongodb';
+import mongoose, { Document, Schema } from 'mongoose'
+import Adress from './Adress'
+import TimeRange from './TimeRange'
+import Offer from './Offer'
+import Review from './Review'
+import { ObjectId } from 'mongodb'
 
 export interface IGym {
   name: String
@@ -14,16 +14,16 @@ export interface IGymModel extends IGym, Document {}
 
 const GymSchema: Schema = new mongoose.Schema({
   name: String,
-  /*highlights: [{ type: String, enum: ["Sauna", "Posing room", "..."] }],
+  highlights: [{ type: String, enum: ['Sauna', 'Posing room', 'Pool'] }],
   websiteLink: String,
   pictures: [String],
   averageRating: Number,
-  address: Adress,
-  openingHours: [TimeRange],
-  offers: [Offer],
-  reviews: [Review],
+  address: Adress.schema,
+  openingHours: [TimeRange.schema],
+  offers: [Offer.schema],
+  reviews: [Review.schema],
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }*/
-});
+  updatedAt: { type: Date, default: Date.now },
+})
 
 export default mongoose.model<IGymModel>('Gym', GymSchema)
