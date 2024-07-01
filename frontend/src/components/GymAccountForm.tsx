f832281cd905c5b9980961a7e71a0191f5be072b
Isabell Helling <isabell.helling@gmail.com>
2024-07-01 13:20:23 +0200

    add password in GymAccountForm

--- frontend/src/components/GymAccountForm.tsx
+++ frontend/src/components/GymAccountForm.tsx
@@ -21,27 +21,38 @@ import { config } from '@/config'
 
 const phoneValidationRegex = /^\+?[1-9]\d{1,14}$/ // E.164 international phone number format
 
-const formSchema = z.object({
-  salutation: z.enum(['Mr.', 'Ms.', 'Diverse'], {
-    required_error: 'Salutation is required.',
-  }),
-  firstName: z
-    .string()
-    .min(2, { message: 'First name must be at least 2 characters.' }),
-  lastName: z
-    .string()
-    .min(2, { message: 'Last name must be at least 2 characters.' }),
-  email: z.string().email({ message: 'Invalid email address.' }),
-  phone: z
-    .string()
-    .min(10, { message: 'Phone number must be at least 10 characters.' })
-    .regex(phoneValidationRegex, {
-      message: 'Please enter a valid phone number.',
+const formSchema = z
+  .object({
+    salutation: z.enum(['Mr.', 'Ms.', 'Diverse'], {
+      required_error: 'Salutation is required.',
     }),
-  address: z
-    .string()
-    .min(5, { message: 'Address must be at least 5 characters.' }),
-})
+    firstName: z
+      .string()
+      .min(2, { message: 'First name must be at least 2 characters.' }),
+    lastName: z
+      .string()
+      .min(2, { message: 'Last name must be at least 2 characters.' }),
+    email: z.string().email({ message: 'Invalid email address.' }),
+    phone: z
+      .string()
+      .min(10, { message: 'Phone number must be at least 10 characters.' })
+      .regex(phoneValidationRegex, {
+        message: 'Please enter a valid phone number.',
+      }),
+    address: z
+      .string()
+      .min(5, { message: 'Address must be at least 5 characters.' }),
+    password: z
+      .string()
+      .min(8, { message: 'Password must be at least 8 characters.' }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: 'Confirm Password must be at least 8 characters.' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  })
 
 export function GymAccountForm() {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -53,6 +64,8 @@ export function GymAccountForm() {
       email: '',
       phone: '',
       address: '',
+      password: '',
+      confirmPassword: '',
     },
   })
 
@@ -198,6 +211,40 @@ export function GymAccountForm() {
             )}
           />
         </div>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="password" {...field} />
+              </FormControl>
+              <FormMessage>
+                {form.formState.errors.password?.message}
+              </FormMessage>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="confirm password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage>
+                {form.formState.errors.confirmPassword?.message}
+              </FormMessage>
+            </FormItem>
+          )}
+        />
         <Button
           type="submit"
           variant="outline"
