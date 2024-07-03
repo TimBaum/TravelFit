b22962f45340cb5189e0fb7f9d1639991bacc35b
Isabell Helling <isabell.helling@gmail.com>
2024-07-03 15:31:30 +0200

    style user account creation page like login page

--- frontend/src/pages/CreateUserAccount.tsx
+++ frontend/src/pages/CreateUserAccount.tsx
@@ -1,26 +1,24 @@
 import UserAccountForm from '@/components/UserAccountForm'
-import { Button } from '@/components/ui/button'
-import { useNavigate } from 'react-router-dom'
 
 function CreateUserAccount() {
-  const navigate = useNavigate()
   return (
-    <>
-      <h1 className="mb-2 font-bold text-5xl">Create your account</h1>
-      <div className="max-w-lg mx-auto">
-        <div className="bg-gray-300 p-10">
+    <div>
+      <h1 className="text-5xl font-bold mb-6 text-center">
+        Create your account
+      </h1>
+      <div className="flex w-full justify-between gap-8">
+        <div className="border border-dashed rounded p-10 pt-4 flex flex-col gap-4 w-full border-primary">
+          <img src="src/assets/illustrations/RollerbladeDoodle.svg" />
+          <div className="font-bold">Stay healthy during your travels</div>
+          <div>Find suitable gyms for your destination</div>
+          <div>Easily compare gyms </div>
+          <div>Get special offers</div>
+        </div>
+        <div className="w-full flex flex-col justify-between">
           <UserAccountForm />
         </div>
-        <div className="flex-1 ml-5">
-          <div className="flex justify-between items-center mt-5">
-            <p>Already have an account?</p>
-            <Button variant="outline" onClick={() => navigate('/login')}>
-              Log in
-            </Button>
-          </div>
-        </div>
       </div>
-    </>
+    </div>
   )
 }
 
