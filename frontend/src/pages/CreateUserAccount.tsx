a6db7373e28b5a20559922561a1cbba0d6089924
Isabell Helling <isabell.helling@gmail.com>
2024-07-03 15:20:59 +0200

    change heading

--- frontend/src/pages/CreateUserAccount.tsx
+++ frontend/src/pages/CreateUserAccount.tsx
@@ -6,7 +6,7 @@ function CreateUserAccount() {
   const navigate = useNavigate()
   return (
     <>
-      <h1 className="mb-2 font-bold text-5xl">Create your user account</h1>
+      <h1 className="mb-2 font-bold text-5xl">Create your account</h1>
       <div className="max-w-lg mx-auto">
         <div className="bg-gray-300 p-10">
           <UserAccountForm />
