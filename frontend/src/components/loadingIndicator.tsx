d9b1fa11cfa84b7636885157c7f3f822b6ff5ee6
Leonie <leonie.popken@tum.de>
2024-07-16 10:54:58 +0200

    fix import

--- frontend/src/components/loadingIndicator.tsx
+++ frontend/src/components/loadingIndicator.tsx
@@ -1,4 +1,4 @@
-import './loadingIndicator.css' // Import the CSS file
+import '../styles/loadingIndicator.css' // Import the CSS file
 
 const LoadingOverlay = () => (
   <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
