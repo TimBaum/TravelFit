50c489afd84cf06e41ff082a9c6f660bd2b51c78
Leonie <leonie.popken@tum.de>
2024-07-16 10:50:30 +0200

    add loading Indicator component and styling for it

---
+++ frontend/src/components/loadingIndicator.tsx
@@ -0,0 +1,12 @@
+import './loadingIndicator.css' // Import the CSS file
+
+const LoadingOverlay = () => (
+  <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="loading-dots">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+)
+export default LoadingOverlay
