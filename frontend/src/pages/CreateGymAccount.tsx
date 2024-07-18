1ef50597decfba105ce68d10898eabf7ab5de95f
Tim Baum <79936444+TimBaum@users.noreply.github.com>
2024-07-18 13:51:04 +0200

    add / before img path

--- frontend/src/pages/CreateGymAccount.tsx
+++ frontend/src/pages/CreateGymAccount.tsx
@@ -12,7 +12,7 @@ function CreateGymAccount() {
       </h1>
       <div className="flex w-full justify-between gap-8">
         <div className="border border-dashed rounded p-10 pt-4 flex flex-col gap-4 w-full border-primary">
-          <img src="src/assets/illustrations/RollerbladeDoodle.svg" />
+          <img src="/src/assets/illustrations/RollerbladeDoodle.svg" />
           <div className="font-bold">Grow your business with TravelFit</div>
           <div>Attract a broader audience</div>
           <div>Spread information about offers and subscriptions</div>
